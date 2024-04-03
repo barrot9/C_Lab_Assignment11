4e377bde06ff538d951755ff4a85cfdc309c962c
bar rot <151166429+barrot9@users.noreply.github.com>
2024-04-03 14:17:43 +0300

    mmn11 init commit

---
+++ my_add/my_add.c
@@ -0,0 +1,104 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* Function prototypes */
+unsigned int my_add(unsigned int a, unsigned int b);
+void display_binary(unsigned int num);
+
+int main(int argc, char *argv[]) {
+    unsigned int a, b, sum;
+
+    /* Check if a filename is provided as a command-line argument */
+    if (argc == 3) {
+        /* Open the file for reading */
+        FILE *file = fopen(argv[1], "r");
+        if (file == NULL) {
+            printf("Error: Could not open file '%s'\n", argv[1]);
+            return 1;
+        }
+
+        /* Read the two unsigned integers from the file */
+        if (fscanf(file, "%u %u", &a, &b) != 2) {
+            printf("Error: Invalid content in file '%s'\n", argv[1]);
+            fclose(file);
+            return 1;
+        }
+
+        /* Close the file */
+        fclose(file);
+    } else if (argc == 1) {
+        /* Input two unsigned integers */
+        printf("Enter the first number (unsigned integer):\n");
+        scanf("%u", &a);
+        printf("Enter the second number (unsigned integer):\n");
+        scanf("%u", &b);
+    } else {
+        /* Incorrect usage */
+        printf("Usage: %s [filename]\n", argv[0]);
+        printf("   or: %s\n", argv[0]);
+        return 1;
+    }
+
+    /* Display binary representation of a and b */
+    printf("\nBinary representation of %u: ", a);
+    display_binary(a);
+    printf("\nBinary representation of %u: ", b);
+    display_binary(b);
+
+    /* Perform binary addition using my_add function */
+    sum = my_add(a, b);
+
+    /* Display binary representation of sum */
+    printf("\n\nBinary representation of sum: ");
+    display_binary(sum);
+
+    /* Display sum in decimal */
+    printf("\nSum: %u\n", sum);
+
+    return 0;
+}
+
+/* Function to add two unsigned integers in binary representation */
+unsigned int my_add(unsigned int a, unsigned int b) {
+    unsigned int result = 0; /* Initialize result */
+    unsigned int carry = 0; /* Initialize carry */
+    unsigned int mask = 1; /* Initialize mask */
+
+    while (a || b || carry) {
+        /* Extract the least significant bits of a, b, and carry */
+        unsigned int bit_a = a & 1;
+        unsigned int bit_b = b & 1;
+
+        /* Calculate sum */
+        unsigned int sum = bit_a ^ bit_b ^ carry;
+
+        /* Update result using sum */
+        result |= (sum * mask); /* Multiply the sum by the current mask value */
+
+        /* Calculate carry */
+        carry = (bit_a & bit_b) | ((bit_a ^ bit_b) & carry);
+
+        /* Move to the next bit position */
+        mask <<= 1;
+
+        /* Shift a and b to the right */
+        a >>= 1;
+        b >>= 1;
+    }
+
+    return result;
+}
+
+/* Function to display an unsigned integer in binary */
+void display_binary(unsigned int num) {
+    unsigned int mask = 1 << (sizeof(unsigned int) * 8 - 1); /* Initialize mask for the most significant bit */
+    int i;
+    for (i = 0; i < sizeof(unsigned int) * 8; ++i) {
+        if (num & mask)
+            printf("1");
+        else
+            printf("0");
+        mask >>= 1;
+    }
+}
+
---
+++ my_suffix/my_suffix.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+#include <string.h>
+
+#define SIZE 128
+
+/* Function prototypes */
+int my_suffix(char str[], char c);
+
+int main() {
+    char str[SIZE]; 
+    char c; 
+    int numSuffixes; 
+    
+    printf("Enter a string: ");
+    scanf("%s", str);
+    
+    printf("Enter a character: ");
+    scanf(" %c", &c);
+    
+    numSuffixes = my_suffix(str, c);
+    
+    printf("Total substrings starting with '%c': %d\n", c, numSuffixes);
+    
+    return 0;
+}
+
+/* Function to print substrings starting with character c */
+int my_suffix(char str[], char c) {
+    int count = 0; 
+    int len = strlen(str); 
+    int i; 
+    
+    printf("Substrings starting with '%c':\n", c);
+    
+    for (i = 0; i < len; i++) { 
+        if (str[i] == c) { 
+            printf("%s\n", &str[i]); 
+            count++; 
+        }
+    }
+    
+    return count; /* Return the total count of suffixes */
+}
+
